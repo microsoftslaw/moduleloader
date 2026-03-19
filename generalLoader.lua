e7c77b7c57c6bbd2df1904d99177c09be341f48c
microsoftslaw <bbackroomspro@gmail.com>
2026-03-19 17:02:13 +0000

    Create generalLoader.lua

---
+++ generalLoader.lua
@@ -0,0 +1,8 @@
+--[[
+Insert a client script in ReplicatedFirst and a server script in ServerScriptService
+]]
+
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local loader = require(replicatedStorage:WaitForChild("Loader"))
+
+loader.loadAll()

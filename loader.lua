12e93146f6bb64b26a2ad04caa8ae6c2546dcf70
microsoftslaw <bbackroomspro@gmail.com>
2026-03-19 17:00:47 +0000

    globalLoader.lua

---
+++ loader.lua
@@ -0,0 +1,49 @@
+--[[
+Put in ReplicatedStorage
+]]
+
+local loader = {}
+
+local runService = game:GetService("RunService")
+
+function loader.loadAll()
+	local replicatedStorage = game:GetService("ReplicatedStorage")
+	local replicatedLoader = replicatedStorage:WaitForChild("Loader")
+	local replicatedScripts = replicatedStorage:WaitForChild("Scripts")
+	
+	local serverAndClientScripts = nil
+	
+	if runService:IsServer() then
+		local serverScriptService = game:GetService("ServerScriptService")
+		local serverScripts = serverScriptService:WaitForChild("Scripts")
+		local serverLoader = serverScriptService:WaitForChild("Loader")
+		
+		serverAndClientScripts = serverScripts
+	elseif runService:IsClient() then
+		local replicatedFirst = game:GetService("ReplicatedFirst")
+		local clientScripts = replicatedFirst:WaitForChild("Scripts")
+		local clientLoader = replicatedFirst:WaitForChild("Loader")
+
+		serverAndClientScripts = clientScripts
+	end
+	
+	for i, module in pairs(replicatedScripts:GetChildren()) do
+		if module:IsA("ModuleScript") then
+			module = require(module)
+			if module.init then
+				module.init()
+			end
+		end
+	end
+	
+	for i, module in pairs(serverAndClientScripts:GetChildren()) do
+		if module:IsA("ModuleScript") then
+			module = require(module)
+			if module.init then
+				module.init()
+			end
+		end
+	end
+end
+
+return loader
